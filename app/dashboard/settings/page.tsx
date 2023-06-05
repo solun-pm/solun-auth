7f22431706aadeea5eac587ac99851e0da33442a
DanielWTE <daniel.iuser@icloud.com>
2023-06-05 21:22:04 +0200

    Added some Dashboard Elements

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -2,10 +2,14 @@
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Navigation from '@/components/navigation'
+import Navigation from '@/components/navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faLock } from '@fortawesome/free-solid-svg-icons';
+import toast, { Toaster } from 'react-hot-toast';
 
 const SettingsPage = () => {
   const router = useRouter();
+
   const [userInfo, setUserInfo] = useState(null) as any;
   const [userDetails, setUserDetails] = useState(null) as any;
 
@@ -63,18 +67,50 @@ const SettingsPage = () => {
     return null;
   }
 
-  const memberSince = new Date(userDetails.createdAt).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
   return (
     <div className="flex items-center justify-center p-6 min-h-screen animate-gradient-x">
       <div className="bg-slate-800 text-white p-5 rounded-lg shadow-md w-full max-w-6xl">
-        {/* <h1 className="text-2xl font-bold mb-2">Dashboard</h1> */}
         <Navigation />
-        {/* @ts-ignore */}
+        <div className="bg-slate-900 p-5 rounded-lg shadow-md max-w-lg mt-4">
+          <h2 className="text-xl font-bold mb-2">Change Password</h2>
+          <form autoComplete="off" onSubmit={handleSubmit}>
+            <div>
+              <div className="mb-4 mt-4">
+                <div className="flex items-center">
+                  <FontAwesomeIcon icon={faLock} className="mr-3 text-gray-400" />
+                  <input
+                    type="password"
+                    name="currentPassword"
+                    className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    placeholder="Current Password"
+                  />
+                </div>
+              </div>
+              <div className="mb-4">
+                <div className="flex items-center">
+                  <FontAwesomeIcon icon={faLock} className="mr-3 text-gray-400" />
+                  <input
+                    type="password"
+                    name="newPassword"
+                    className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    placeholder="New Password"
+                  />
+                </div>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
