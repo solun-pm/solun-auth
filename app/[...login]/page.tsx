bd3eb3b96608d8e9061cab50d95a66a3668faf8e
DanielWTE <daniel.iuser@icloud.com>
2023-06-12 18:41:51 +0200

    Outsourced Login 2FA to a modal

--- app/[...login]/page.tsx
+++ app/[...login]/page.tsx
@@ -1,37 +1,51 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock, faUser, faCircleNotch, faKey } from '@fortawesome/free-solid-svg-icons';
-import { useRouter } from 'next/navigation';
-import toast, { Toaster } from 'react-hot-toast';
+import React, { useState, useEffect, useRef, Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLock,
+  faUser,
+  faCircleNotch,
+  faKey,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+import { Dialog, Transition } from "@headlessui/react";
 
 const LoginPage = ({ params }: { params: { login: string[] } }) => {
-
   const router = useRouter();
 
   const redirectService = params.login[1];
-  const [serviceProvider, setServiceProvider] = useState('');
+  const [serviceProvider, setServiceProvider] = useState("");
 
   // TODO: Move this to a config file.
-  const domains = ['@solun.pm', '@6crypt.com', '@seal.pm', '@xolus.de', '@cipher.pm'];
+  const domains = [
+    "@solun.pm",
+    "@6crypt.com",
+    "@seal.pm",
+    "@xolus.de",
+    "@cipher.pm",
+  ];
 
-  const [suggestedDomain, setSuggestedDomain] = useState('');
+  const [suggestedDomain, setSuggestedDomain] = useState("");
   const [showSuggestion, setShowSuggestion] = useState(false);
-  const [show2FA, setShow2FA] = useState(false);
-  const [twoFACode, setTwoFACode] = useState('');
+  const [twoFACode, setTwoFACode] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   const emailInputRef = useRef(null);
 
   const handleEmailChange = (e: any) => {
     const value = e.target.value;
-    setFormData({ ...formData, 'fqe': value });
-    const atIndex = value.indexOf('@');
+    setFormData({ ...formData, fqe: value });
+    const atIndex = value.indexOf("@");
 
     if (atIndex !== -1) {
       const inputDomain = value.slice(atIndex);
 
-      const matchedDomain = domains.find(domain => domain.startsWith(inputDomain));
+      const matchedDomain = domains.find((domain) =>
+        domain.startsWith(inputDomain)
+      );
       if (matchedDomain) {
         setSuggestedDomain(matchedDomain);
         setShowSuggestion(true);
@@ -41,25 +55,25 @@ const LoginPage = ({ params }: { params: { login: string[] } }) => {
     } else {
       setShowSuggestion(false);
     }
-  }
+  };
 
   const applySuggestedDomain = () => {
     if (emailInputRef.current) {
-      const newValue = formData.fqe.split('@')[0] + suggestedDomain;
-      setFormData({ ...formData, 'fqe': newValue });
+      const newValue = formData.fqe.split("@")[0] + suggestedDomain;
+      setFormData({ ...formData, fqe: newValue });
       setShowSuggestion(false);
     }
-  }
+  };
 
   useEffect(() => {
-    if(redirectService === 'mail') {
-      setServiceProvider('Mail');  
+    if (redirectService === "mail") {
+      setServiceProvider("Mail");
     }
   }, [redirectService]);
 
   const [formData, setFormData] = useState({
-    fqe: '',
-    password: '',
+    fqe: "",
+    password: "",
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -67,27 +81,27 @@ const LoginPage = ({ params }: { params: { login: string[] } }) => {
   const handleChange = (e: any) => {
     const name = e.target.name;
     setFormData({ ...formData, [name]: e.target.value });
-  }
+  };
 
   const isValidForm = () => {
     return formData.fqe && formData.password;
-  }  
+  };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     setIsSubmitting(true);
-  
+
     if (!isValidForm()) {
       setIsSubmitting(false);
       return;
     }
-  
+
     try {
-      const response = await fetch('/api/user/login', {
-        method: 'POST',
+      const response = await fetch("/api/user/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           fqe: formData.fqe,
@@ -95,31 +109,32 @@ const LoginPage = ({ params }: { params: { login: string[] } }) => {
           service: serviceProvider,
         }),
       });
-  
+
       const data = await response.json();
-  
+
       if (!response.ok) {
         toast.error(data.message);
         return;
       }
 
       if (data.two_fa) {
-        setShow2FA(true);
+        setShowModal(true);
         setIsSubmitting(false);
         return;
       }
 
       if (data.redirect) {
-        if (data.service === 'Mail'){
+        if (data.service === "Mail") {
           window.location.href = data.redirectUrl;
         }
       } else {
-        toast.success('Login successful!');
-        localStorage.setItem('jwt', data.token);
-        router.push('/dashboard');
+        toast.success("Login successful!");
+        localStorage.setItem("jwt", data.token);
+        router.push("/dashboard");
       }
     } catch (error) {
-      toast.error('An error occurred. Please try again later.');
+      toast.error("An error occurred. Please try again later.");
+      setIsSubmitting(false);
     } finally {
       setIsSubmitting(false);
     }
@@ -135,10 +150,10 @@ const LoginPage = ({ params }: { params: { login: string[] } }) => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('/api/2fa/verify', {
-        method: 'POST',
+      const response = await fetch("/api/2fa/verify", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           fqe: formData.fqe,
@@ -157,100 +172,193 @@ const LoginPage = ({ params }: { params: { login: string[] } }) => {
       }
 
       if (data.redirect) {
-        if (data.service === 'Mail'){
+        if (data.service === "Mail") {
           window.location.href = data.redirectUrl;
         }
       } else {
-        toast.success('Login successful!');
-        localStorage.setItem('jwt', data.token);
-        router.push('/dashboard');
+        toast.success("Login successful!");
+        localStorage.setItem("jwt", data.token);
+        router.push("/dashboard");
       }
     } catch (error) {
-      toast.error('An error occurred. Please try again later.');
+      toast.error("An error occurred. Please try again later.");
       setIsSubmitting(false);
     }
   };
 
   return (
     <div className="flex items-center justify-center py-8 px-2 min-h-screen animate-gradient-x">
-      <Toaster
-        position="top-right"
-      />
+      <Toaster position="top-right" />
       <div className="bg-slate-800 text-white p-5 rounded-lg shadow-md w-full max-w-md">
+        {showModal && (
+          <Transition appear show={showModal} as={Fragment}>
+            <Dialog
+              as="div"
+              className="fixed inset-0 z-10 overflow-y-auto"
+              onClose={() => setShowModal(false)}
+            >
+              <div className="min-h-screen px-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+                </Transition.Child>
+                <span
+                  className="inline-block h-screen align-middle"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-slate-900 shadow-xl rounded-2xl">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-white"
+                    >
+                      This account is secured by a Two-Factor-Authenticator
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <div className="flex items-center">
+                        <FontAwesomeIcon
+                          icon={faKey}
+                          className="mr-3 text-gray-400"
+                        />
+                        <input
+                          type="text"
+                          name="twoFACode"
+                          onChange={handleTwoFACodeChange}
+                          className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                          placeholder="Enter your 2FA Code here"
+                        />
+                      </div>
+                    </div>
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="bg-gray-500 hover:bg-gray-600 text-white font-bold px-3 py-3 rounded transition duration-200 mr-2"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
+                        onClick={handle2FASubmit}
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </div>
+            </Dialog>
+          </Transition>
+        )}
         <h1 className="text-2xl font-bold mb-2">Login</h1>
-        {redirectService === 'mail' ?
-          <p className="mb-4">Welcome back! Please login to continue to your {serviceProvider} account.</p>
-        :
+        {redirectService === "mail" ? (
+          <p className="mb-4">
+            Welcome back! Please login to continue to your {serviceProvider}{" "}
+            account.
+          </p>
+        ) : (
           <p className="mb-5">Welcome back! Please login to continue.</p>
-        }
-        <form onSubmit={show2FA ? handle2FASubmit : handleSubmit} autoComplete="off">
+        )}
+        <form onSubmit={handleSubmit} autoComplete="off">
           <div className="mb-1 flex items-center">
-            <FontAwesomeIcon icon={faUser} className="mr-3 text-gray-400"/>
-            <input 
-              type="text" 
-              name="fqe" 
-              onChange={handleEmailChange} 
-              className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
+            <FontAwesomeIcon icon={faUser} className="mr-3 text-gray-400" />
+            <input
+              type="text"
+              name="fqe"
+              onChange={handleEmailChange}
+              className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               placeholder="E-Mail Address"
               ref={emailInputRef}
               value={formData.fqe}
             />
           </div>
-          {showSuggestion && 
-              <div 
-                className="text-gray-400 text-xs cursor-pointer ml-7 mb-2" 
-                onClick={applySuggestedDomain}
-              >
-                Do you mean: <span className="text-blue-500">{formData.fqe.split('@')[0] + suggestedDomain}</span>?
-          </div>}
+          {showSuggestion && (
+            <div
+              className="text-gray-400 text-xs cursor-pointer ml-7 mb-2"
+              onClick={applySuggestedDomain}
+            >
+              Do you mean:{" "}
+              <span className="text-blue-500">
+                {formData.fqe.split("@")[0] + suggestedDomain}
+              </span>
+              ?
+            </div>
+          )}
           <div className="mb-4 mt-4">
             <div className="flex items-center">
-              <FontAwesomeIcon icon={faLock} className="mr-3 text-gray-400"/>
-              <input 
-                type="password" 
-                name="password" 
-                onChange={handleChange} 
-                className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
+              <FontAwesomeIcon icon={faLock} className="mr-3 text-gray-400" />
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 placeholder="Password"
               />
             </div>
           </div>
-          {show2FA && (
-            <div className="mb-4 mt-4">
-              <div className="flex items-center">
-                <FontAwesomeIcon icon={faKey} className="mr-3 text-gray-400"/>
-                <input 
-                  type="numer"
-                  name="twoFACode" 
-                  onChange={handleTwoFACodeChange} 
-                  className="bg-slate-950 text-white w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none" 
-                  placeholder="2FA Code"
-                />
-              </div>
-            </div>
-          )}
-          <button 
-            type="submit" 
+          <button
+            type="submit"
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-3 rounded transition duration-200 flex justify-center items-center"
             disabled={!isValidForm() || isSubmitting}
           >
-            {isSubmitting && 
-              <FontAwesomeIcon 
-                icon={faCircleNotch} 
+            {isSubmitting && (
+              <FontAwesomeIcon
+                icon={faCircleNotch}
                 className="animate-spin mr-2"
               />
-            }
+            )}
             Login
           </button>
         </form>
         <div className="mt-5 text-center">
-          <p className="mb-4">Don't have an account? <a href="/signup" className="text-blue-500 hover:text-blue-600">Register</a></p>
+          <p className="mb-4">
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-500 hover:text-blue-600">
+              Register
+            </a>
+          </p>
           <hr className="h-px border-0 bg-gray-500" />
-          <p className="text-sm mt-4 text-slate-400">By logging in, you agree to our <a href={"https://"+process.env.NEXT_PUBLIC_MAIN_DOMAIN+"/terms"} className="text-blue-500 hover:text-blue-600">Terms of Service</a> and <a href={"https://"+process.env.NEXT_PUBLIC_MAIN_DOMAIN+"/privacy"} className="text-blue-500 hover:text-blue-600">Privacy Policy</a>.</p>
+          <p className="text-sm mt-4 text-slate-400">
+            By logging in, you agree to our{" "}
+            <a
+              href={"https://" + process.env.NEXT_PUBLIC_MAIN_DOMAIN + "/terms"}
+              className="text-blue-500 hover:text-blue-600"
+            >
+              Terms of Service
+            </a>{" "}
+            and{" "}
+            <a
+              href={
+                "https://" + process.env.NEXT_PUBLIC_MAIN_DOMAIN + "/privacy"
+              }
+              className="text-blue-500 hover:text-blue-600"
+            >
+              Privacy Policy
+            </a>
+            .
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
