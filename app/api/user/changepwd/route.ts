5a3616bcced0eeba6df4667be429522b3277c932
DanielWTE <daniel.iuser@icloud.com>
2023-06-16 15:54:50 +0200

    Added Change Password to Mailserver API

--- app/api/user/changepwd/route.ts
+++ app/api/user/changepwd/route.ts
@@ -4,12 +4,14 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 import { decrypt, encrypt } from "@/utils/encryption";
 import { comparePassword, hashPassword } from "@/utils/hash";
+const { MailcowApiClient } = require("@/utils/mail");
 
 export async function POST(request: Request) {
     try {
         const res = await request.json();
 
         await dbConnect();
+        const mcc = new MailcowApiClient(process.env.MAILSERVER_BASEURL, process.env.MAILSERVER_API_KEY);
 
         let user_id = res.user_id;
         let currentPassword = res.currentPassword;
@@ -17,6 +19,10 @@ export async function POST(request: Request) {
 
         if (currentPassword === '' || newPassword === '') {
             return NextResponse.json({ message: "Please fill out all fields" }, { status: 400 });
+        }
+
+        if (newPassword.length < 6 || !newPassword.match(/[^a-zA-Z0-9]/)) {
+            return NextResponse.json({ message: "Password must be at least 6 characters long and contain at least 1 special character" }, { status: 400 });
         }
 
         const user = await findOneDocument(User, { user_id: user_id });
@@ -30,9 +36,26 @@ export async function POST(request: Request) {
         }
 
         const decryptedPrivateKey = decrypt(user.private_key, currentPassword);
-        const decryptedTwoFASecret = decrypt(user.two_fa_secret, currentPassword);
         const encryptedPrivateKey = encrypt(decryptedPrivateKey, newPassword);
+
+        // Set rate limit for user
+        const updateMailbox = await mcc.updateMailbox({
+            attr: {
+                force_pw_update: 0,
+                password: newPassword,
+                password2: newPassword,
+              },
+              items: [
+                user.fqe,
+              ],
+        });
+
+        if (!updateMailbox) {
+            return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+        }
+
         if (user.two_fa) {
+            const decryptedTwoFASecret = decrypt(user.two_fa_secret, currentPassword);
             const encryptedTwoFASecret = encrypt(decryptedTwoFASecret, newPassword);
 
             await updateOneDocument(User, { user_id: user_id }, { two_fa_secret: encryptedTwoFASecret });
