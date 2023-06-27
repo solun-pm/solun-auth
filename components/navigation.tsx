c3cdbe3969de2d84c07885a52c10bfe942e82582
DanielWTE <daniel.iuser@icloud.com>
2023-06-27 20:21:45 +0200

    Updated navigation with alias and domains

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -19,35 +19,65 @@ const Navigation = () => {
     router.push("/dashboard/settings");
   };
 
+  const goToAliases = () => {
+    router.push("/dashboard/aliases");
+  };
+
+  const goToDomains = () => {
+    router.push("/dashboard/domains");
+  };
+
   return (
-    <>
-      <nav className="flex justify-start bg-slate-950 p-4 mb-4 rounded-lg shadow-xl">
-        <button
-          onClick={goToOverview}
-          className={`mr-4 text-white font-bold py-2 px-4 rounded transition-all ${
-            pathname === "/dashboard" ? "bg-blue-500" : "hover:bg-blue-500"
-          }`}
-        >
-          Overview
-        </button>
-        <button
-          onClick={goToSettings}
-          className={`text-white font-bold py-2 px-4 rounded transition-all ${
-            pathname === "/dashboard/settings"
-              ? "bg-blue-500"
-              : "hover:bg-blue-500"
-          }`}
-        >
-          Settings
-        </button>
-        <button
-          onClick={handleLogout}
-          className="ml-auto text-white font-bold py-2 px-4 rounded transition-all hover:bg-blue-500"
-        >
-          Logout
-        </button>
-      </nav>
-    </>
+      <>
+        <nav className="flex flex-col md:flex-row md:justify-between items-center md:space-x-4 space-y-2 md:space-y-0 bg-slate-950 p-4 mb-4 rounded-lg shadow-xl">
+          <div className="flex flex-col md:flex-row justify-start md:space-x-4 space-y-2 md:space-y-0 w-full">
+            <button
+              onClick={goToOverview}
+              className={`text-white font-bold py-2 px-4 rounded transition-all w-full md:w-auto text-center ${
+                pathname === "/dashboard" ? "bg-blue-500" : "hover:bg-blue-500"
+              }`}
+            >
+              Overview
+            </button>
+            <button
+              onClick={goToSettings}
+              className={`text-white font-bold py-2 px-4 rounded transition-all w-full md:w-auto text-center ${
+                pathname === "/dashboard/settings"
+                  ? "bg-blue-500"
+                  : "hover:bg-blue-500"
+              }`}
+            >
+              Settings
+            </button>
+            <button
+              onClick={goToAliases}
+              className={`text-white font-bold py-2 px-4 rounded transition-all w-full md:w-auto text-center ${
+                pathname === "/dashboard/aliases"
+                  ? "bg-blue-500"
+                  : "hover:bg-blue-500"
+              }`}
+            >
+              Aliases
+            </button>
+            <button
+              onClick={goToDomains}
+              className={`text-white font-bold py-2 px-4 rounded transition-all w-full md:w-auto text-center ${
+                pathname === "/dashboard/domains"
+                  ? "bg-blue-500"
+                  : "hover:bg-blue-500"
+              }`}
+            >
+              Domains
+            </button>
+          </div>
+          <button
+            onClick={handleLogout}
+            className="text-white font-bold py-2 px-4 rounded transition-all w-full md:w-auto text-center md:text-left hover:bg-blue-500 mt-4 md:mt-0"
+          >
+            Logout
+          </button>
+        </nav>
+      </>
   );
 };
 
