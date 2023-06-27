f8954eaddc375a6744aa648d728bf4ee126cd344
DanielWTE <daniel.iuser@icloud.com>
2023-06-27 23:39:38 +0200

    added delete alias function

--- components/aliases/aliasCard.tsx
+++ components/aliases/aliasCard.tsx
@@ -1,7 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMailBulk, faTrash } from "@fortawesome/free-solid-svg-icons";
+import toast from "react-hot-toast";
 
-const AliasCard = ({ aliasName, domain }: any) => {
+const AliasCard = ({ userInfo, aliasName, domain, refreshAliases }: any) => {
+
+  const handleDeleteAlias = async (aliasName: string, domain: string) => {
+    const fqa = aliasName + domain;
+    const res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/user/delete_alias", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: userInfo.user_id,
+        fqa: fqa,
+      }),
+    });
+    if (!res.ok) {
+      toast.error('Something went wrong');
+      return;
+    }
+    toast.success('Alias deleted successfully');
+    refreshAliases();
+  }
+
   return (
     <div className="bg-slate-900 rounded p-4 shadow-md flex flex-col justify-between space-y-4 h-full text-center">
       <div>
@@ -11,7 +33,9 @@ const AliasCard = ({ aliasName, domain }: any) => {
           <p className="text-gray-400">{domain}</p>
         </div>
       </div>
-      <button className="text-white bg-red-500 py-2 px-4 rounded font-bold hover:bg-red-600 transition-all self-stretch text-center">
+      <button 
+        onClick={() => handleDeleteAlias(aliasName, domain)}
+        className="text-white bg-red-500 py-2 px-4 rounded font-bold hover:bg-red-600 transition-all self-stretch text-center">
         <FontAwesomeIcon icon={faTrash} className="h-5 w-5 inline-block mr-1" /> Delete
       </button>
     </div>
