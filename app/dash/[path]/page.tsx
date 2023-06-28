551be314eec11a05689eefdc4dd7a13d69325af1
DanielWTE <daniel.iuser@icloud.com>
2023-06-28 21:23:06 +0200

    added lazy loading

--- app/dash/[path]/page.tsx
+++ app/dash/[path]/page.tsx
@@ -1,55 +1,48 @@
 "use client";
 
-import React, { useEffect, useState, Suspense } from "react";
-import { useRouter } from "next/navigation";
+import React, { Suspense, lazy } from "react";
+import { useRouter } from "next/router";
 import Navigation from "@/components/navigation";
-
-import OverviewPage from "@/pages/subpages/overview";
-import SettingsPage from "@/pages/subpages/settings";
-import AliasesPage from "@/pages/subpages/aliases";
-
 import { Toaster } from "react-hot-toast";
-
 import { useFetchUserInfo } from "@/hooks/fetchUserInfo";
 import Loader from "@/components/loader";
 
-const MainPage = ({params}: {params: { path: string }}) => {
+const OverviewPage = lazy(() => import("@/pages/subpages/overview"));
+const SettingsPage = lazy(() => import("@/pages/subpages/settings"));
+const AliasesPage = lazy(() => import("@/pages/subpages/aliases"));
+
+const MainPage = () => {
   const router = useRouter();
-  const [Subpage, setSubpage] = useState(null);
-
   const { userInfo, userDetails } = useFetchUserInfo() as any;
-
-  useEffect(() => {
-    switch (params.path) {
-      case 'overview':
-        setSubpage(<OverviewPage /> as any);
-        break;
-      case 'settings':
-        setSubpage(<SettingsPage /> as any);
-        break;
-      case 'aliases':
-        setSubpage(<AliasesPage /> as any);
-        break;
-      default:
-        router.push('/');
-    }
-  }, [params.path, router]);
+  
+  const { path } = router.query;
+  
+  let Subpage;
+  switch (path) {
+    case 'overview':
+      Subpage = OverviewPage;
+      break;
+    case 'settings':
+      Subpage = SettingsPage;
+      break;
+    case 'aliases':
+      Subpage = AliasesPage;
+      break;
+    default:
+      router.push('/');
+  }
 
   if (!userInfo || !userDetails) {
     return null;
   }
 
-  if (!Subpage) {
-    return null;
-  }
-
   return (
     <div className="flex items-center justify-center min-h-screen p-6 animate-gradient-x">
-    <Toaster position="top-right" />
+      <Toaster position="top-right" />
       <div className="bg-slate-800 text-white p-5 rounded-lg shadow-md w-full max-w-6xl">
         <Navigation />
         <Suspense fallback={<Loader />}>
-          {Subpage}
+          {Subpage && <Subpage />}
         </Suspense>
       </div>
     </div>
