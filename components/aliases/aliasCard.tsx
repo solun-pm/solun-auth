5ac796302f97e225bb61f6a14bfa5a4aff813680
DanielWTE <daniel.iuser@icloud.com>
2023-06-30 16:02:13 +0200

    test

--- components/aliases/aliasCard.tsx
+++ components/aliases/aliasCard.tsx
@@ -51,19 +51,19 @@ const AliasCard = ({ userInfo, aliasName, domain, refreshAliases }: any) => {
           <p className="text-gray-400">{domain}</p>
         </div>
       </div>
-      <div className="flex space-x-2">
+      <div className="flex gap-2">
         <CopyToClipboard text={aliasName + domain} onCopy={handleCopy}>
           <button 
             className={`py-2 px-4 rounded font-bold transition-all self-stretch text-center ${copySuccess ? 'text-white bg-green-500 hover:bg-green-600' : 'text-white bg-blue-500 hover:bg-blue-600'}`} 
             disabled={copySuccess}
           >
-            <FontAwesomeIcon icon={copySuccess ? faCheck : faCopy} className="h-5 w-5 inline-block mr-1" /> {copySuccess ? 'Copied' : 'Copy'}
+            <FontAwesomeIcon icon={copySuccess ? faCheck : faCopy} className="h-5 inline-block mr-1" /> {copySuccess ? 'Copied' : 'Copy'}
           </button>
         </CopyToClipboard>
         <button 
           onClick={() => handleDeleteAlias(aliasName, domain)}
           className="text-white bg-red-500 py-2 px-4 rounded font-bold hover:bg-red-600 transition-all self-stretch text-center">
-          <FontAwesomeIcon icon={faTrash} className="h-5 w-5 inline-block mr-1" /> Delete
+          <FontAwesomeIcon icon={faTrash} className="h-5 inline-block mr-1" /> Delete
         </button>
       </div>
     </div>
