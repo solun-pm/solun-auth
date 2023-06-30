285a65be580e4d7ae2294915a43773ef2de70f8b
DanielWTE <daniel.iuser@icloud.com>
2023-06-30 22:25:03 +0200

    added recovery code generation

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLock,
@@ -14,6 +14,8 @@ import {
 import { useRouter } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
 import { FriendlyCaptcha } from "@/components/captcha";
+import { Dialog, Transition } from "@headlessui/react";
+import { hashPassword, generateRecoveryCode } from "solun-general-package";
 
 const { version } = require("../../package.json");
 
@@ -40,6 +42,8 @@ const RegisterPage = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirmVisible, setPasswordConfirmVisible] = useState(false);
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [showRecoveryModal, setShowRecoveryModal] = useState(false);
+  const [recoveryCode, setRecoveryCode] = useState("");
 
   useEffect(() => {
     let timer: any;
@@ -124,8 +128,12 @@ const RegisterPage = () => {
       return;
     }
 
+    const recoveryCode = generateRecoveryCode();
+    setRecoveryCode(recoveryCode);
+    setShowRecoveryModal(true);
+
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/user/create", {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/user/create-testing-", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -136,6 +144,7 @@ const RegisterPage = () => {
           password: password,
           confirmPassword: passwordConfirm,
           solution: solution,
+          recoveryCode: hashPassword(recoveryCode),
         }),
       });
 
@@ -310,6 +319,67 @@ const RegisterPage = () => {
           </p>
         </div>
       </div>
+      {showRecoveryModal && (
+    <Transition appear show={showRecoveryModal} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={() => setShowRecoveryModal(false)}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+          </Transition.Child>
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-slate-900 shadow-xl rounded-2xl">
+              <Dialog.Title
+                as="h3"
+                className="text-lg font-medium leading-6 text-white"
+              >
+                Your recovery code
+              </Dialog.Title>
+              <div className="mt-2">
+                <p className="text-sm text-slate-300">
+                  Write down this recovery code. With this key, you can reset your account password and your 2FA status, if applicable.                </p>
+                <p className="mt-4 font-bold">{recoveryCode}</p>
+              </div>
+              <div className="mt-4">
+                <button
+                  type="button"
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
+                  onClick={() => setShowRecoveryModal(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+    )}
     </div>
   );
 };
