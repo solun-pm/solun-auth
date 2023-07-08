bfd9b8ef3ada51644bb934b880edd2fdc2fb7890
DanielWTE <daniel.iuser@icloud.com>
2023-07-08 22:35:37 +0200

    Fixed dialog

--- components/domains/addDomainDialog.tsx
+++ components/domains/addDomainDialog.tsx
@@ -64,15 +64,41 @@ const AddDomainDialog = ({ isOpen, closeModal, userInfo, userDetails }: any) => 
   };
 
   return (
-    <Transition show={true} as={Fragment}>
+    <Transition show={isOpen} as={Fragment}>
     <Dialog
       as="div"
       className="fixed inset-0 z-10 overflow-y-auto"
       initialFocus={cancelButtonRef}
-      static
       open={isOpen}
       onClose={closeModal}
     >
+      <div className="px-4 min-h-screen text-center">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+        </Transition.Child>
+        <span
+          className="inline-block h-screen align-middle"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
         <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
           {/* More Dialog code... */}
           {step === 1 && (
@@ -146,8 +172,10 @@ const AddDomainDialog = ({ isOpen, closeModal, userInfo, userDetails }: any) => 
             </div>
           )}
         </div>
-      </Dialog>
-    </Transition>
+        </Transition.Child>
+      </div>
+    </Dialog>
+  </Transition>
   );
 };
 
