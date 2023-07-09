483ac539cb7e97579cfab47f636d746112ddf1bf
DanielWTE <daniel.iuser@icloud.com>
2023-07-09 12:11:47 +0200

    Improved dialog design for domain

--- components/domains/addDomainDialog.tsx
+++ components/domains/addDomainDialog.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useRef, useState, useEffect } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faGlobe, faCircleNotch, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faGlobe, faCircleNotch, faCheckCircle, faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
 import toast from "react-hot-toast";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
@@ -11,6 +11,9 @@ const AddDomainDialog = ({ isOpen, closeModal, userInfo, userDetails }: any) => 
   const [step, setStep] = useState(1);
   const [submitButtonLoading, setSubmitButtonLoading] = useState(false);
   const [domain, setDomain] = useState("");
+  const [copyIcon, setCopyIcon] = useState(true);
+  const [timeoutId, setTimeoutId] = useState(null);
+
 
   const checkDomain = async () => {
     if (domain === '') {
@@ -64,124 +67,173 @@ const AddDomainDialog = ({ isOpen, closeModal, userInfo, userDetails }: any) => 
     setDnsData(data.dnsData);
   };
 
-  const updateStatusDomain = async (status: any) => {
-    // Your async fetch logic here
+  const closeDialog = () => {
+    closeModal();
+    setDnsData([]);
+    setStep(1);
+    setDomain("");
   };
 
   return (
     <Transition show={isOpen} as={Fragment}>
-    <Dialog
-      as="div"
-      className="fixed inset-0 z-10 overflow-y-auto"
-      initialFocus={cancelButtonRef}
-      open={isOpen}
-      onClose={closeModal}
-    >
-      <div className="px-4 min-h-screen text-center">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
-        </Transition.Child>
-        <span
-          className="inline-block h-screen align-middle"
-          aria-hidden="true"
-        >
-          &#8203;
-        </span>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-        <div className="inline-block align-bottom bg-slate-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-          {/* More Dialog code... */}
-          {step === 1 && (
-            <div className="my-4">
-              <label className="text-slate-300 text-md">Domain</label>
-              <div className="mb-4 flex items-center mt-1">
-                <FontAwesomeIcon icon={faGlobe} className="mr-3 text-gray-400" />
-                <input
-                  type="text"
-                  className="bg-slate-950 text-slate-300 p-2 pr-7 w-full focus:outline-none focus:border-transparent"
-                  placeholder="Enter your domain"
-                  value={domain}
-                  onChange={e => setDomain(e.target.value)}
-                />
-              </div>
-              <div className="mt-4">
-                <button
-                  onClick={() => checkDomain()}
-                  type="submit"
-                  disabled={submitButtonLoading}
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
-                >
-                  {submitButtonLoading && (
-                    <FontAwesomeIcon
-                      icon={faCircleNotch}
-                      className="animate-spin mr-2"
-                    />
-                  )}
-                  Next
-                </button>
-              </div>
-            </div>
-          )}
-          {step === 2 && (
-            <div className="my-4">
-              <label className="text-slate-300 text-md">DNS Data</label>
-              {dnsData && dnsData.map((entry: any, index: any) => (
-                <div key={index} className="mb-4">
-                  <p>Type: {entry.type}</p>
-                  <p>Name: {entry.name}</p>
-                  <p>Data: {entry.data}</p>
-                  <CopyToClipboard text={`${entry.type} ${entry.name} ${entry.data}`}>
-                    <button>Copy</button>
-                  </CopyToClipboard>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        open={isOpen}
+        onClose={closeModal}
+      >
+        <div className="px-4 min-h-screen text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+          </Transition.Child>
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+          <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-slate-900 shadow-xl rounded-2xl">
+            {step === 1 && (
+              <div className="my-4">
+                <h1 className="text-white text-2xl">Add a new Domain</h1>
+                <p className="text-slate-300 text-md mt-2 mb-4 break-words">
+                  Please enter a domain to proceed.
+                  This domain will be added to your account and you can start creating mailboxes and aliases for it.
+                  In the next step you will see the DNS records you need to add to your domain.
+                </p>
+                <label className="text-slate-300 text-md">Domain</label>
+                <div className="mb-4 flex items-center mt-1">
+                  <FontAwesomeIcon icon={faGlobe} className="mr-3 text-gray-400" />
+                  <input
+                    type="text"
+                    className="bg-slate-950 text-slate-300 p-2 pr-7 w-full focus:outline-none focus:border-transparent"
+                    placeholder="Enter your domain"
+                    value={domain}
+                    onChange={e => setDomain(e.target.value)}
+                  />
                 </div>
-              ))}
-              <div className="mt-4">
-                <button
-                  onClick={() => updateStatusDomain('pending')}
-                  type="submit"
-                  disabled={submitButtonLoading}
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
-                >
-                  {submitButtonLoading && (
-                    <FontAwesomeIcon
-                      icon={faCircleNotch}
-                      className="animate-spin mr-2"
-                    />
-                  )}
-                  Complete
-                </button>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="bg-gray-500 hover:bg-gray-600 text-white font-bold px-3 py-3 rounded transition duration-200 mt-4 mr-2"
+                    onClick={closeDialog}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => checkDomain()}
+                    type="submit"
+                    disabled={submitButtonLoading}
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
+                  >
+                    {submitButtonLoading && (
+                      <FontAwesomeIcon
+                        icon={faCircleNotch}
+                        className="animate-spin mr-2"
+                      />
+                    )}
+                    Next
+                  </button>
+                </div>
               </div>
-            </div>
-          )}
-          {step === 3 && (
-            <div>
-              <FontAwesomeIcon icon={faCheckCircle} />
-              <p>Domain successfully added</p>
-              <p>Your domain is currently being verified, you will see its status in your Domain Dashboard.</p>
-              <button onClick={() => {}}>Close</button>
-            </div>
-          )}
+            )}
+            {step === 2 && (
+              <div className="my-4">
+                <h1 className="text-white text-2xl">DNS Data</h1>
+                <p className="text-slate-300 text-md mt-2 mb-4 break-words">
+                  Please add the following DNS records to your domain.
+                  If you don't know how to do this, please contact your domain provider or check out our <a href="https://docs.solun.pm/" target="_blank" className="text-blue-500">documentation</a>.
+                  After that click on the Complete button.
+                </p>
+                {dnsData && dnsData.map((entry: any, index: any) => (
+                  <div key={index} className="mb-4 p-3 bg-slate-800 rounded-lg">
+                    <div className="flex items-center">
+                      <span className="rounded-full bg-gray-500 px-3 py-1 text-white text-xs font-bold mr-3">{entry.type}</span>
+                      <h3 className="text-slate-300">{entry.name}</h3>
+                    </div>
+                    <div className="mt-2 bg-slate-900 p-2 rounded">
+                      <div className="flex items-center justify-between">
+                        <p className="text-white">{entry.data}</p>
+                        <CopyToClipboard text={`${entry.type} ${entry.name} ${entry.data}`}>
+                          <button 
+                            onClick={() => {
+                              setCopyIcon(false);
+                              clearTimeout(timeoutId as any);
+                              const id = setTimeout(() => setCopyIcon(true), 3000);
+                              setTimeoutId(id as any);
+                            }}
+                          >
+                            <FontAwesomeIcon icon={copyIcon ? faCopy : faCheck} className="text-green-500" />
+                          </button>
+                        </CopyToClipboard>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="bg-gray-500 hover:bg-gray-600 text-white font-bold px-3 py-3 rounded transition duration-200 mt-4 mr-2"
+                    onClick={closeDialog}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => setStep(3)}
+                    type="submit"
+                    disabled={submitButtonLoading}
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-3 py-3 rounded transition duration-200"
+                  >
+                    {submitButtonLoading && (
+                      <FontAwesomeIcon
+                        icon={faCircleNotch}
+                        className="animate-spin mr-2"
+                      />
+                    )}
+                    Complete
+                  </button>
+                </div>
+              </div>
+            )}
+            {step === 3 && (
+              <div>
+                <FontAwesomeIcon icon={faCheckCircle} />
+                <h1 className="text-white text-2xl">Domain successfully added</h1>
+                <p className="text-slate-300 text-md mt-2 mb-4 break-words">
+                  Your domain is currently being verified, you will see its status in your Domain Dashboard.
+                  If it's active you can start creating mailboxes and aliases for it.
+                  <br/>
+                  We will send you an email once your domain is active.
+                </p>
+                <button onClick={() => closeDialog()}>Finish</button>
+              </div>
+            )}
+          </div>
+          </Transition.Child>
         </div>
-        </Transition.Child>
-      </div>
-    </Dialog>
-  </Transition>
-  );
+      </Dialog>
+    </Transition>
+  );  
 };
 
 export default AddDomainDialog;
