52a3cfffe2f95afb73185122555bbd2927a7605a
DanielWTE <daniel.iuser@icloud.com>
2023-07-09 14:38:01 +0200

    Updated order

--- pages/subpages/domains.tsx
+++ pages/subpages/domains.tsx
@@ -58,7 +58,7 @@ const DomainsPage = () => {
             <>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-4">
             {domainsToShow.map((domain: any, index: any) => (
-                <DomainCard key={index} domain={domain.domain} status={domain.status} mailboxes={domain.mailboxes} aliases={domain.aliases} mailbox_cap={domain.mailbox_cap} alias_cap={domain.alias_cap} />
+              <DomainCard key={index} domain={domain.domain} status={domain.status} mailboxes={domain.mailbox_count} aliases={domain.alias_count} mailbox_cap={domain.mailbox_cap} alias_cap={domain.alias_cap} />
             ))}
             </div>
             <div className="flex justify-center mt-4">
