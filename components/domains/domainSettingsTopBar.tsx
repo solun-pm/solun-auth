8bc0d11be92611c024c46656d934318e24f4954f
DanielWTE <daniel.iuser@icloud.com>
2023-07-16 17:09:44 +0200

    added catch all ui

--- components/domains/domainSettingsTopBar.tsx
+++ components/domains/domainSettingsTopBar.tsx
@@ -1,15 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMailBulk, faBolt, faTrash, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMailBulk, faBolt, faTrash, faArrowLeft, faCocktail } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import AddMailboxDialog from "../mailbox/addMailboxDialog";
 import toast from 'react-hot-toast';
 import AreYouSureBro from '@/components/misc/areYouSureBro';
+import EnableCatchAllDialog from "./enableCatchAllDialog";
 
-const  DomainSettingsTopBar = ({ domain_id, userInfo, userDetails, mailboxCount, rateLimit, refreshMailboxes, domain }: any) => {
+const  DomainSettingsTopBar = ({ domain_id, userInfo, userDetails, mailboxCount, rateLimit, refreshMailboxes, domain, catch_all }: any) => {
   const router = useRouter();
   const [isAddMailboxDialogOpen, setIsAddMailboxDialogOpen] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isCatchAllDialogOpen, setIsCatchAllDialogOpen] = useState(false);
 
   const confirmDelete = async (confirmed: boolean) => {
     if (confirmed) {
@@ -50,7 +52,29 @@ const  DomainSettingsTopBar = ({ domain_id, userInfo, userDetails, mailboxCount,
     } else {
       setIsDialogOpen(false);
     }
-  };  
+  };
+
+  const disableCatchAll = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/user/domain/disable_catch_all", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: userInfo.user_id,
+        domain_id: domain_id,
+      }),
+    });
+
+    if (!res.ok) {
+      toast.error('Something went wrong');
+      return;
+    }
+
+    toast.success('Catch-all has been disabled');
+    setIsCatchAllDialogOpen(false);
+  };
+
   return (
     <>
       <div className="flex flex-col sm:flex-row items-center justify-between my-4">
@@ -77,6 +101,21 @@ const  DomainSettingsTopBar = ({ domain_id, userInfo, userDetails, mailboxCount,
           >
             <FontAwesomeIcon icon={faPlus} className="mr-2"/> Add Mailbox
           </button>
+          {!catch_all && (
+          <button 
+            className="bg-blue-500 text-white px-4 py-2 rounded flex hover:bg-blue-600 transition-all items-center w-full sm:w-auto mt-4 sm:mt-0"
+            onClick={() => setIsCatchAllDialogOpen(true)}
+          >
+            <FontAwesomeIcon icon={faCocktail} className="mr-2"/> Enable Catch-All
+          </button>
+          )} : {(
+            <button 
+            className="bg-red-500 text-white px-4 py-2 rounded flex hover:bg-red-600 transition-all items-center w-full sm:w-auto mt-4 sm:mt-0"
+            onClick={() => disableCatchAll()}
+          >
+            <FontAwesomeIcon icon={faCocktail} className="mr-2"/> Disable Catch-All
+          </button>
+          )}
         </div>
         <button 
           className="bg-red-500 text-white px-4 py-2 rounded flex hover:bg-red-600 transition-all items-center w-full sm:w-auto mt-4 sm:mt-0"
@@ -85,6 +124,7 @@ const  DomainSettingsTopBar = ({ domain_id, userInfo, userDetails, mailboxCount,
           <FontAwesomeIcon icon={faTrash} className="mr-2"/> Delete Domain
         </button>
         <AddMailboxDialog userInfo={userInfo} isOpen={isAddMailboxDialogOpen} closeModal={() => setIsAddMailboxDialogOpen(false)} domain={domain} refreshMailboxes={refreshMailboxes} />
+        <EnableCatchAllDialog userInfo={userInfo} isOpen={isCatchAllDialogOpen} closeModal={() => setIsCatchAllDialogOpen(false)} domain_id={domain_id} />
       </div>
       <AreYouSureBro 
       isOpen={isDialogOpen} 
